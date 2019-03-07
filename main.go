87fea5a8000f6bf638561d127dd57d6ac7731c15
Ibis Ibrahim <ibis@etu.unistra.fr>
2019-03-07 16:43:18 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example configured by Ibis"
+	title := "Jenkins X golang http example configured by Ibrahim"
 
 	from := ""
 	if r.URL != nil {
